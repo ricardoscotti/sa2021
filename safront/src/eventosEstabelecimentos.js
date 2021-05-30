2c2e018b4a9c04a9f416301b217c49bcb58f888d
marcosvsantos08 <marcos_v_santos@estudante.sc.senai.br>
2021-05-30 16:44:29 -0300

    maps

---
+++ safront/src/eventosEstabelecimentos.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    useColorScheme,
+    View,
+    FlatList,
+    Button,
+  } from 'react-native';
+import api from './services/axios';
